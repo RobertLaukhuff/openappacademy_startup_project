95b9a7ed4004a481a321ec1b1cfc8b0273d97e7b
Robert Laukhuff <robert@laukcoder.com>
2019-09-20 23:57:20 -0400

    Build constructor and other methods
    Finish class build tomorrow

--- lib/startup.rb
+++ lib/startup.rb
@@ -1,5 +1,37 @@
-require "employee"
+require_relative "employee"
 
 class Startup
 
+  attr_reader :name, :funding, :salaries, :employees
+
+  def initialize(name, funding, salaries)
+    @name = name
+    @funding = funding
+    @salaries = salaries
+    @employees = []
+  end
+
+  def valid_title?(title)
+    @salaries.keys.include?(title)
+  end
+
+  def >(other_startup)
+    self.funding > other_startup.funding
+  end
+
+  def hire(name, title)
+    if !valid_title?(title)
+      raise "#{title} is not a valid title"
+    else
+      @employees << Employee.new(name, title)
+    end
+  end
+
+  def size
+    @employees.length
+  end
+
+  def pay_employee(employee)
+    # continue here
+  end
 end
